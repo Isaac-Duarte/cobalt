b4c0adda7e7e851bbce698fdc9d0ecb29bab5079
c272 <dev@c272.org>
2024-04-26 15:06:44 +0100

    build: Add profile support, clean script.

--- build.sh
+++ build.sh
@@ -9,6 +9,7 @@ set -e
 ## CONSTANTS ##
 ###############
 
+RED='\033[0;31m'
 ORANGE='\033[0;33m'
 CYAN='\033[0;36m'
 GREEN='\033[1;32m'
@@ -19,7 +20,8 @@ NC='\033[0m'
 #####################
 
 # The build profile we're building with.
-PROFILE="debug"
+BUILD_PROFILE="dev"
+BUILD_PROFILE_DIR="debug"
 
 ################
 ## ENTRYPOINT ##
@@ -28,10 +30,17 @@ PROFILE="debug"
 # Fetch the script directory.
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 
+# Check if we've got a build mode specified.
+if [[ $1 == "release" ]]; then
+    echo -e "${CYAN}Building Cobalt in release mode.${NC}"
+    BUILD_PROFILE="release"
+    BUILD_PROFILE_DIR="release"
+fi
+
 # Build each of the crates in turn.
 echo -e "${CYAN}Building compiler...${NC}"
 cd "${SCRIPT_DIR}/crates/compiler/"
-cargo build
+cargo build --profile ${BUILD_PROFILE}
 
 # If the architecture is aarch64, we build with PAC/BTI.
 arch=$(uname -m)
@@ -43,19 +52,33 @@ if [[ $arch == "aarch64" ]]; then
     rustup component add rust-src --toolchain nightly-aarch64-unknown-linux-gnu
     RUSTFLAGS="-Z branch-protection=bti,pac-ret,leaf" cargo build \
         -Zbuild-std="core,alloc,panic_abort" \
-        --target aarch64-unknown-linux-gnu
+        --target aarch64-unknown-linux-gnu \
+        --profile ${BUILD_PROFILE}
     rustup default stable
 else
     echo -e "${ORANGE}Non-aarch64 architecture detected, building intrinsics with security features OFF.${NC}"
-    cargo build
+    cargo build --profile ${BUILD_PROFILE}
 fi
 
-# Copy the build artifacts out.
+# Prepare to copy the build artifacts out.
 echo -e "${CYAN}Copying build artifacts...${NC}"
 BUILD_DIR="${SCRIPT_DIR}/build"
 mkdir -p "$BUILD_DIR"
-cp "${SCRIPT_DIR}/crates/intrinsics/target/${PROFILE}/libcobalt_intrinsics.a" "${BUILD_DIR}"
-cp "${SCRIPT_DIR}/crates/compiler/target/${PROFILE}/cobalt" "${BUILD_DIR}"
+
+# Search for the intrinsics artifact.
+find . -name libcobalt_intrinsics.a | while read -r line ; do
+    if [[ $line =~ "$BUILD_PROFILE_DIR" ]]; then
+        cp $line "${BUILD_DIR}"
+        break;
+    fi
+done
+if [[ -z "$(find $BUILD_DIR -name libcobalt_intrinsics.a)" ]]; then
+    echo -e "${RED}Failed to find intrinsics library artifact.${NC}"
+    exit -1
+fi
+
+# Copy the compiler executable artifact.
+cp "${SCRIPT_DIR}/crates/compiler/target/${BUILD_PROFILE_DIR}/cobalt" "${BUILD_DIR}"
 
 # Done!
 echo -e "${GREEN}Build complete.${NC}"
---
+++ clean.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+set -e
+
+# Cleans build artifacts from the Cobalt compiler and intrinsics libraries.
+# Author: dev@c272.org
+
+###############
+## CONSTANTS ##
+###############
+
+ORANGE='\033[0;33m'
+CYAN='\033[0;36m'
+GREEN='\033[1;32m'
+NC='\033[0m'
+
+#####################
+## BUILD VARIABLES ##
+#####################
+
+# The argument for the build profile we're cleaning.
+BUILD_PROFILE_ARG=""
+
+################
+## ENTRYPOINT ##
+################
+
+# Fetch the script directory.
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+
+# Check if we've got a build profile specified.
+if [[ $1 == "dev" ]]; then
+    echo -e "${CYAN}Cleaning artifacts for debug mode only.${NC}"
+    BUILD_PROFILE_ARG="--profile dev"
+fi
+if [[ $1 == "release" ]]; then
+    echo -e "${CYAN}Cleaning artifacts for release mode only.${NC}"
+    BUILD_PROFILE_ARG="--profile release"
+fi
+
+# Clean each of the crates in turn.
+echo -e "${CYAN}Cleaning compiler artifacts...${NC}"
+cd "${SCRIPT_DIR}/crates/compiler/"
+cargo clean $BUILD_PROFILE_ARG
+
+echo -e "${CYAN}Cleaning intrinsics library artifacts...${NC}"
+cd "${SCRIPT_DIR}/crates/intrinsics/"
+cargo clean $BUILD_PROFILE_ARG
+
+# Clean copied build artifacts in ./build.
+echo -e "${CYAN}Cleaning copied build artifacts...${NC}"
+BUILD_DIR="${SCRIPT_DIR}/build"
+rm -rf "$BUILD_DIR"
+
+# Done!
+echo -e "${GREEN}Clean complete.${NC}"
